9d78958fb2f17bb3a536b5e633472a0af148cfa8
hardsp0rk <107102839+hardsp0rk@users.noreply.github.com>
2022-06-23 17:02:18 +0800

    Add push-pull ownership management

--- src/PegToken.sol
+++ src/PegToken.sol
@@ -3,10 +3,12 @@
 pragma solidity ^0.8.0;
 
 import "openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "./OwnableUpgradeable.sol";
 
-contract PegToken is ERC20PermitUpgradeable {
+contract PegToken is ERC20PermitUpgradeable, OwnableUpgradeable {
     function initialize(string memory _name, string memory _symbol) public initializer {
         __Context_init();
+        __Ownable_init();
         __ERC20_init(_name, _symbol);
         __ERC20Permit_init(_name);
     }
