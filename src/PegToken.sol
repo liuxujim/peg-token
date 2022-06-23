073f6746b13b4803ee3be7285c25b897eda6cd6b
hardsp0rk <107102839+hardsp0rk@users.noreply.github.com>
2022-06-23 17:29:57 +0800

    Add Pausable

--- src/PegToken.sol
+++ src/PegToken.sol
@@ -3,13 +3,31 @@
 pragma solidity ^0.8.0;
 
 import "openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "openzeppelin-contracts-upgradeable/contracts/security/PausableUpgradeable.sol";
 import "./OwnableUpgradeable.sol";
 
-contract PegToken is ERC20PermitUpgradeable, OwnableUpgradeable {
+contract PegToken is ERC20PermitUpgradeable, OwnableUpgradeable, PausableUpgradeable {
     function initialize(string memory _name, string memory _symbol) public initializer {
         __Context_init();
         __Ownable_init();
         __ERC20_init(_name, _symbol);
         __ERC20Permit_init(_name);
+        __Pausable_init();
+    }
+
+    /**
+     * @dev Triggers stopped state.
+     * Can only be called by the current owner.
+     */
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @dev Returns to normal state.
+     * Can only be called by the current owner.
+     */
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
