ea93a08c92ccc616b79db02765bc48558c5ebac0
hardsp0rk <107102839+hardsp0rk@users.noreply.github.com>
2022-06-23 17:20:51 +0800

    Add mint/burn (onlyOwner)

--- src/PegToken.sol
+++ src/PegToken.sol
@@ -7,12 +7,40 @@ import "openzeppelin-contracts-upgradeable/contracts/security/PausableUpgradeabl
 import "./OwnableUpgradeable.sol";
 
 contract PegToken is ERC20PermitUpgradeable, OwnableUpgradeable, PausableUpgradeable {
+
+    event Mint(address indexed caller, address indexed to, uint256 amount);
+    event Burn(address indexed caller, address indexed from, uint256 amount);
+
     function initialize(string memory _name, string memory _symbol) public initializer {
         __Context_init();
         __Ownable_init();
         __ERC20_init(_name, _symbol);
         __ERC20Permit_init(_name);
         __Pausable_init();
+    }
+
+    /** 
+     * @dev See {ERC20-_mint}.
+     * @param amount Mint amount
+     * @return True if successful
+     * Can only be called by the current owner.
+     */
+    function mint(uint256 amount) external onlyOwner returns (bool) {
+        _mint(_msgSender(), amount);
+        emit Mint(_msgSender(), _msgSender(), amount);
+        return true;
+    }
+
+    /**
+     * @dev See {ERC20-_burn}.
+     * @param amount Burn amount
+     * @return True if successful
+     * Can only be called by the current owner.
+     */
+    function burn(uint256 amount) public onlyOwner returns (bool) {
+        _burn(_msgSender(), amount);
+        emit Burn(_msgSender(), _msgSender(), amount);
+        return true;
     }
 
     /**
